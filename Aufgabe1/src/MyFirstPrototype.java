430238cba210cdb400172e27ded2f5680f43c540
RoberHaiek <rober.haiek@gmail.com>
2023-07-21 20:37:00 +0300

    Adding functional fixes

--- Aufgabe1/src/MyFirstPrototype.java
+++ Aufgabe1/src/MyFirstPrototype.java
@@ -1,36 +1,46 @@
 package version0;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.*;
 public class MyFirstPrototype {
 
-	private File i;
-	private File y;
-
-	public MyFirstPrototype(File filea, File fileb) {
-		this.i = filea;
-		this.y = fileb;
+	private File firstFile;
+	private File secondFile;
+	private static final Logger logger = Logger.getLogger(MyFirstPrototype.class.getName());
+	public MyFirstPrototype(File firstFile, File secondFile) {
+		this.firstFile = firstFile;
+		this.secondFile = secondFile;
 	}
+	public boolean isIdentical() throws IOException {
 
-	public boolean doIt() throws IOException {
+		try(BufferedReader firstFileBufferReader = new BufferedReader(new FileReader(firstFile, StandardCharsets.UTF_8));
+			BufferedReader secondFileBufferReader = new BufferedReader(new FileReader(secondFile, StandardCharsets.UTF_8))){
 
-		BufferedReader rg = new BufferedReader(new FileReader(i));
-		BufferedReader rr = new BufferedReader(new FileReader(y));
-		String r = null;
-		String g = null;
+			String firstFileLine = "";
+			String secondFileLine = "";
+			while (firstFileLine != null && secondFileLine != null && firstFileLine.compareTo(secondFileLine) == 0) {
+				firstFileLine = firstFileBufferReader.readLine();
+				secondFileLine = secondFileBufferReader.readLine();
+			}
 
-		while (true) {
-			if ((r = rg.readLine()) == null || (g = rr.readLine()) == null
-			|| r.compareTo(g) != 0)
-				break;
+			String loggerMessage = firstFileLine == null && secondFileLine == null ? Constants.FILES_IDENTICAL : Constants.FILES_NOT_IDENTICAL;
+			logger.info(loggerMessage);
+
+			return (firstFileLine == null && secondFileLine == null);
+
+		}catch (FileNotFoundException e){
+			handleException(e.getMessage());
+			throw e;
 		}
-
-		if (r == null && g == null)
-			return true;
-
-		return false;
+	}
+	private void handleException(String errorMessage){
+		logger.severe(
+				new MessageCode.FileNotFoundBuilder()
+						.withSourceFileName(firstFile.getName())
+						.withTargetFileName(secondFile.getName())
+						.withErrorMessage(errorMessage)
+						.build()
+						.getMessage());
 	}
 
 }
