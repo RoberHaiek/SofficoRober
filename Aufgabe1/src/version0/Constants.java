b6f1672019a756ec5aa487a9e1fc426048fc5a25
RoberHaiek <rober.haiek@gmail.com>
2023-07-22 11:11:31 +0300

    Adding a constant

--- Aufgabe1/src/version0/Constants.java
+++ Aufgabe1/src/version0/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
     public static String COPIED_SUCCESSFULLY = "Source file content copied to target file successfully";
     public static String FILES_IDENTICAL = "Files are identical.";
     public static String FILES_NOT_IDENTICAL = "Files are not identical.";
+    public static String SHA_256_UNAVAILABLE = "SHA-256 algorithm not available.";
     public static int BUFFER_SIZE = 4096;
     public static int THREAD_SLEEP_TIME_MS = 10000;
 }
