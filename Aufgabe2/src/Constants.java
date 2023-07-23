9c3c3774aea41cec4296b5f08e7874515dc7dc6b
RoberHaiek <rober.haiek@gmail.com>
2023-07-23 15:40:25 +0300

    Adding cosmetic changes

---
+++ Aufgabe2/src/Constants.java
@@ -0,0 +1,8 @@
+public class Constants {
+
+    public static String NEW_FILE_DETECTED = "New XML file detected: ";
+    public static String PARSING_SUCCESSFUL = "Parsing done successfully.";
+    public static String VALIDATION_FAILED = "XML validation failed.";
+    public static String VALIDATION_FILE_PATH = "Aufgabe2/resources/material_export.xsd";
+    public static int THREAD_SLEEP_TIME = 1000;
+}
