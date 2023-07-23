c793affdcec2fa34ed7a7d527a21f4d6abe196ba
RoberHaiek <rober.haiek@gmail.com>
2023-07-23 15:24:48 +0300

    Fixing exercise 1 question 2

--- Aufgabe2/src/ShortText.java
+++ Aufgabe2/src/ShortText.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class ShortText {
+public class ShortText {
+    private String lang;
+    private String text;
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public ShortText lang(String lang){
+        this.lang = lang;
+        return this;
+    }
+
+    public ShortText text(String text){
+        this.text = text;
+        return this;
+    }
 }
--- Aufgabe2/src/XmlFileReader.java
+++ Aufgabe2/src/XmlFileReader.java
@@ -9,37 +9,22 @@ import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.*;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
-public class Test {
+public class XmlFileReader {
 
     private final Path folderPath;
     private final int period = 5;
-    private static final Logger logger = Logger.getLogger(Test.class.getName());
+    private static final Logger logger = Logger.getLogger(XmlFileReader.class.getName());
 
-    public Test(Path folderPath) {
+    public XmlFileReader(Path folderPath) {
         this.folderPath = folderPath;
-    }
-
-    public void testing() throws ParserConfigurationException, IOException, SAXException {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse(new File( "file" ));
-        Schema schema = null;
-        try {
-
-            String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
-            SchemaFactory factory2 = SchemaFactory.newInstance(language);
-            schema = factory2.newSchema(new File("name"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Validator validator = schema.newValidator();
-        validator.validate(new DOMSource(document));
     }
 
     public void startWatching() {
@@ -47,25 +32,90 @@ public class Test {
 
         Runnable folderWatcherTask = () -> {
             try {
-                WatchService watchService = FileSystems.getDefault().newWatchService();
-                folderPath.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
-
-                WatchKey key = watchService.poll(1, TimeUnit.SECONDS);
-                if (key != null) {
-                    for (WatchEvent<?> event : key.pollEvents()) {
-                        Path filePath = (Path) event.context();
-                        if (filePath.toString().endsWith(".xml")) {
-                            logger.info("New XML file detected: " + filePath);
-                            // Do your processing for the new XML file here
+                Files.walkFileTree(folderPath, new SimpleFileVisitor<>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                        if (file.toString().endsWith(".xml")) {
+                            logger.info("New XML file detected: " + file);
+                            Path fullPath = folderPath.resolve(file);
+                            try {
+                                readXMLFile(fullPath);
+                                logger.info("parsing done");
+                            } catch (ParserConfigurationException e) {
+                                throw new RuntimeException(e);
+                            } catch (SAXException e) {
+                                throw new RuntimeException(e);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
                         }
+                        return FileVisitResult.CONTINUE;
                     }
-                    key.reset();
-                }
-            } catch (IOException | InterruptedException e) {
+                });
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         };
 
         executor.scheduleAtFixedRate(folderWatcherTask, 0, period, TimeUnit.SECONDS);
+    }
+
+    private void readXMLFile(Path filePath) throws IOException, ParserConfigurationException, SAXException, InterruptedException {
+
+        List<Material> materials = new ArrayList<>();
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
+        Document document = builder.parse(filePath.toFile());
+        document.getDocumentElement().normalize();
+
+        validator(document);
+
+        NodeList nList = document.getElementsByTagName("record");
+
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+
+            Node node = nList.item(temp);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+                Element eElement = (Element) node;
+                Material material = new Material();
+                material.setMatNr(eElement.getElementsByTagName("matnr").item(0).getTextContent());
+                material.setPrice(Double.parseDouble(eElement.getElementsByTagName("price").item(0).getTextContent()));
+                addDescriptionElements(eElement.getElementsByTagName("description"), material);
+                materials.add(material);
+            }
+        }
+
+        Thread.sleep(1000);
+    }
+
+    private void addDescriptionElements(NodeList descriptionNodes, Material material){
+        List<ShortText> shortTexts = new ArrayList<>();
+        for(int i=0; i<descriptionNodes.getLength(); i++){
+            Element element = (Element) descriptionNodes.item(i);
+            NodeList textNodes = element.getElementsByTagName("text");
+            for (int k = 0; k < textNodes.getLength(); k++) {
+                Element textElement = (Element) textNodes.item(k);
+                String lang = textElement.getAttribute("lang");
+                String value = textElement.getAttribute("value");
+                ShortText shortText = new ShortText().lang(lang).text(value);
+                shortTexts.add(shortText);
+            }
+        }
+        material.setShortText(shortTexts);
+    }
+    private void validator(Document document) throws IOException {
+        try {
+            String xsdFile = "Aufgabe2/resources/material_export.xsd";
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(new File(xsdFile));
+
+            Validator validator = schema.newValidator();
+            validator.validate(new DOMSource(document));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IOException("XML validation failed.");
+        }
     }
 }
