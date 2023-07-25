e54d56133764904c9cb3376577f6be7493f08ba7
Rober Haiek <43522261+RoberHaiek@users.noreply.github.com>
2023-07-25 13:26:04 +0300

    Update Material.java

--- Aufgabe2/src/Material.java
+++ Aufgabe2/src/Material.java
@@ -3,14 +3,12 @@ import java.util.List;
 public class Material {
     private String matNr;
     private double price;
+    private List<ShortText> shortTexts;
 
     public static String RECORD = "record";
     public static String MATNR = "matnr";
     public static String PRICE = "price";
     public static String DESCRIPTION = "description";
-
-
-    private List<ShortText> shortTexts;
 
     public String getMatNr() {
         return matNr;
